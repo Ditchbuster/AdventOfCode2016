6649208191bddda1d923c322f18e5cb42c907415
Ditchbuster <trioman008@hotmail.com>
2016-12-01 08:32:40 -0700

    complete part 1

--- day1/day1p1.py
+++ day1/day1p1.py
@@ -1,3 +1,31 @@
 f = open('puzzle.txt')
 for line in f:
-    print(line, end='')
+    direction = 0 #0 north 1 east 2 south 3 west
+    x = 0 #+north -south
+    y = 0 #+east -west
+    dis = 0
+
+    #print(line, end='')
+    path = line.split(', ')
+    for turn in path:
+        #print(turn[0])        
+        if turn[0] == 'R':
+            direction += 1
+            if direction == 4:
+                direction = 0
+        if turn[0] == 'L':
+            direction -= 1
+            if direction == -1:
+                direction = 3
+        dis = int(turn[1:]) #get everything after the 0th char
+        if direction == 0:
+            x += dis
+        elif direction == 1:
+            y += dis
+        elif direction == 2:
+            x -= dis
+        elif direction == 3:
+            y -= dis
+
+    print(str(abs(x) + abs(y)))
+    #print(str(direction))
