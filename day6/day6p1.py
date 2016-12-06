2baf38a7fa989a5cf4f7004a3f6d0490517274a4
Chris P <trioman008@hotmail.com>
2016-12-05 22:18:08 -0700

    day 6 part 1 and 2 complete
    423 and 388

---
+++ day6/day6p1.py
@@ -0,0 +1,15 @@
+f = open("input.txt")
+spot = [0]*26
+mesg = []
+
+for line in f:
+    while len(mesg) < len(line):
+        #print('adding spot')
+        mesg.append(spot.copy())
+    for i in range(len(line)):
+        #print(line[i])
+        if(line[i].isalpha()):
+            mesg[i][ord(line[i])-97] += 1
+
+for s in mesg:
+    print(chr(s.index(max(s))+97))
